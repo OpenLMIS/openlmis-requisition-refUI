40c0468c5f234810e9e3018ec4538c9577a39298
Nick Reid <nickreid@nickreid.com>
2017-01-31 12:14:54 -0800

    OLMIS-1072: Removed a commited a merge conflict

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,6 @@
 module.exports = function(grunt) {
-<<<<<<< HEAD
-    var configSetup = require('../dev-ui/tasks/config.js');
-
+    var configSetup = require('dev-ui/tasks/config.js');
     configSetup(grunt);
 
-    grunt.loadNpmTasks('openlmis-ui');
-
-=======
-    grunt.loadNpmTasks('openlmis-ui');
-
-    grunt.option('applicationDirectories', [
-        '/openlmis/openlmis-requisition-ui'
-        ]);
->>>>>>> 458733be78d66af110dbc43fdf2e1a1cdda94296
+    grunt.loadNpmTasks('dev-ui');
 };
